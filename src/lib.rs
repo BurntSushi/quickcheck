2f35e31df0962b669cf43af25882fd433c047580
Andrew Gallant <jamslam@gmail.com>
2014-03-14 01:56:47 -0400

    Support recovery from task failures.
    (rustc will do it for us, but quickcheck has to do it too so that
    we can shrink appropriately.)
    
    Sadly, it seems like this prevents stack closures from satisfying
    the Testable trait.

--- src/lib.rs
+++ src/lib.rs
@@ -25,9 +25,9 @@ mod arbitrary;
 mod tester {
     use std::fmt::Show;
     use std::iter;
-    use std::task;
     use rand::task_rng;
     use super::{Arbitrary, Gen, ObjIter, gen};
+    use tester::trap::safe;
 
     /// Default size hint used in `quickcheck` for sampling from a random
     /// distribution.
@@ -73,13 +73,13 @@ mod tester {
     pub fn quickcheck_config<A: Testable, G: Gen>(c: Config, g: &mut G, f: A) {
         match quicktest_config(c, g, f) {
             Ok(ntests) => debug!("[quickcheck] Passed {:u} tests.", ntests),
-            Err(err) => fail!(err),
+            Err(r) => fail!(r.failed_msg()),
         }
     }
 
     /// Like `quickcheck`, but returns either the number of tests passed
     /// or a witness of failure.
-    pub fn quicktest<A: Testable>(f: A) -> Result<uint, ~str> {
+    pub fn quicktest<A: Testable>(f: A) -> Result<uint, TestResult> {
         let g = &mut gen(task_rng(), DEFAULT_SIZE);
         quicktest_config(DEFAULT_CONFIG, g, f)
     }
@@ -87,7 +87,8 @@ mod tester {
     /// Like `quickcheck_config`, but returns either the number of tests passed
     /// or a witness of failure.
     pub fn quicktest_config<A: Testable, G: Gen>
-                           (c: Config, g: &mut G, f: A) -> Result<uint, ~str> {
+                           (c: Config, g: &mut G, f: A)
+                           -> Result<uint, TestResult> {
         let mut ntests: uint = 0;
         for _ in iter::range(0, c.max_tests) {
             if ntests >= c.tests {
@@ -98,9 +99,7 @@ mod tester {
                 Pass => ntests = ntests + 1,
                 Discard => continue,
                 Fail => {
-                    return Err(format!(
-                        "[quickcheck] TEST FAILED. Arguments: ({})",
-                        r.arguments.connect(", ")));
+                    return Err(*r)
                 }
             }
         }
@@ -129,6 +128,7 @@ mod tester {
     pub struct TestResult {
         priv status: Status,
         priv arguments: ~[~str],
+        priv err: ~str,
     }
 
     /// Whether a test has passed, failed or been discarded.
@@ -142,19 +142,31 @@ mod tester {
         /// Produces a test result that indicates the current test has failed.
         pub fn failed() -> ~TestResult { TestResult::from_bool(false) }
 
+        /// Produces a test result that indicates failure from a runtime
+        /// error.
+        pub fn error(msg: &str) -> ~TestResult {
+            let mut r = TestResult::from_bool(false);
+            r.err = msg.to_owned();
+            r
+        }
+
         /// Produces a test result that instructs `quickcheck` to ignore it.
         /// This is useful for restricting the domain of your properties.
         /// When a test is discarded, `quickcheck` will replace it with a
         /// fresh one (up to a certain limit).
         pub fn discard() -> ~TestResult {
-            ~TestResult { status: Discard, arguments: ~[] }
+            ~TestResult { status: Discard, arguments: ~[], err: ~"", }
         }
 
         /// Converts a `bool` to a `~TestResult`. A `true` value indicates that
         /// the test has passed and a `false` value indicates that the test
         /// has failed.
         pub fn from_bool(b: bool) -> ~TestResult {
-            ~TestResult { status: if b { Pass } else { Fail }, arguments: ~[] }
+            ~TestResult {
+                status: if b { Pass } else { Fail },
+                arguments: ~[],
+                err: ~"",
+            }
         }
 
         /// Returns `true` if and only if this test result describes a failing
@@ -163,6 +175,25 @@ mod tester {
             match self.status {
                 Fail => true,
                 Pass|Discard => false,
+            }
+        }
+
+        /// Returns `true` if and only if this test result describes a failing
+        /// test as a result of a run time error.
+        pub fn is_error(&self) -> bool {
+            return self.is_failure() && self.err.len() > 0
+        }
+
+        fn failed_msg(&self) -> ~str {
+            if self.err.len() == 0 {
+                return format!(
+                    "[quickcheck] TEST FAILED. Arguments: ({})",
+                    self.arguments.connect(", "))
+            } else {
+                return format!(
+                    "[quickcheck] TEST FAILED (runtime error). \
+                    Arguments: ({})\nError: {}",
+                    self.arguments.connect(", "), self.err)
             }
         }
     }
@@ -185,7 +216,7 @@ mod tester {
     /// I'll hopefully add it. (As of now, it would be very difficult to
     /// add your own implementation outside of `quickcheck`, since the
     /// functions that do shrinking are not public.)
-    pub trait Testable {
+    pub trait Testable : Send {
         fn result<G: Gen>(&self, &mut G) -> ~TestResult;
     }
 
@@ -200,121 +231,91 @@ mod tester {
     }
 
     impl<A: Testable> Testable for Result<A, ~str> {
-        fn result<G: Gen>(&self, _: &mut G) -> ~TestResult {
+        fn result<G: Gen>(&self, g: &mut G) -> ~TestResult {
             match *self {
-                Ok(_) => TestResult::passed(),
-                Err(_) => TestResult::failed(),
+                Ok(ref r) => r.result(g),
+                Err(ref err) => TestResult::error(*err),
             }
         }
     }
 
-    // I should really figure out how to use macros. This is painful.
-    // N.B. This isn't needed in Haskell because it's currying by default!
-    // Perhaps there is a way to circumvent this in Rust too (without macros),
-    // but I'm not sure.
-
-    impl<A: Testable> Testable for 'static || -> A {
+    impl<T: Testable> Testable for fn() -> T {
         fn result<G: Gen>(&self, g: &mut G) -> ~TestResult {
-            apply0(g, || (*self)())
+            shrink(g, Zero::<(), (), (), T>(*self))
         }
     }
 
-    impl<A: AShow, B: Testable> Testable for 'static |A| -> B {
+    impl<A: AShow, T: Testable> Testable for fn(A) -> T {
         fn result<G: Gen>(&self, g: &mut G) -> ~TestResult {
-            apply1(g, |a| (*self)(a))
+            shrink(g, One::<A, (), (), T>(*self))
         }
     }
 
-    impl<A: AShow, B: AShow, C: Testable> Testable for 'static |A, B| -> C {
+    impl<A: AShow, B: AShow, T: Testable> Testable for fn(A, B) -> T {
         fn result<G: Gen>(&self, g: &mut G) -> ~TestResult {
-            apply2(g, |a, b| (*self)(a, b))
+            shrink(g, Two::<A, B, (), T>(*self))
         }
     }
 
-    impl<A: AShow, B: AShow, C: AShow, D: Testable>
-        Testable for 'static |A, B, C| -> D {
+    impl<A: AShow, B: AShow, C: AShow, T: Testable>
+        Testable for fn(A, B, C) -> T {
         fn result<G: Gen>(&self, g: &mut G) -> ~TestResult {
-            apply3(g, |a, b, c| (*self)(a, b, c))
+            shrink(g, Three::<A, B, C, T>(*self))
         }
     }
 
-    impl<A: Testable> Testable for fn() -> A {
-        fn result<G: Gen>(&self, g: &mut G) -> ~TestResult {
-            apply0(g, || (*self)())
-        }
+    enum Fun<A, B, C, T> {
+        Zero(fn() -> T),
+        One(fn(A) -> T),
+        Two(fn(A, B) -> T),
+        Three(fn(A, B, C) -> T),
     }
 
-    impl<A: AShow, B: Testable> Testable for fn(A) -> B {
-        fn result<G: Gen>(&self, g: &mut G) -> ~TestResult {
-            apply1(g, |a| (*self)(a))
-        }
-    }
-
-    impl<A: AShow, B: AShow, C: Testable> Testable for fn(A, B) -> C {
-        fn result<G: Gen>(&self, g: &mut G) -> ~TestResult {
-            apply2(g, |a, b| (*self)(a, b))
-        }
-    }
-
-    impl<A: AShow, B: AShow, C: AShow, D: Testable>
-        Testable for fn(A, B, C) -> D {
-        fn result<G: Gen>(&self, g: &mut G) -> ~TestResult {
-            apply3(g, |a, b, c| (*self)(a, b, c))
-        }
-    }
-
-    // The following apply functions are used to abstract over the different
-    // function types `fn` and `||`.
-
-    fn apply0<A: Testable, G: Gen>(g: &mut G, fun: || -> A) -> ~TestResult {
-        shrink((), (), (), |_: (), _: (), _: ()| fun().result(g))
-    }
-
-    fn apply1<A: AShow, B: Testable, G: Gen>
-             (g: &mut G, fun: |a: A| -> B) -> ~TestResult {
-        let a = arby(g);
-        shrink(a, (), (), |a: A, _: (), _: ()| {
-            let mut r = fun(a.clone()).result(g);
-            if r.is_failure() {
-                r.arguments = ~[a.to_str()];
+    impl<A: AShow, B: AShow, C: AShow, T: Testable> Fun<A, B, C, T> {
+        fn call<G: Gen>(self, g: &mut G,
+                        a: Option<&A>, b: Option<&B>, c: Option<&C>)
+                       -> ~TestResult {
+            match self {
+                Zero(f) => safe(proc() { f() }).result(g),
+                One(f) => {
+                    let oa = ~a.unwrap().clone();
+                    let mut r = safe(proc() { f(*oa) }).result(g);
+                    if r.is_failure() {
+                        r.arguments = ~[a.to_str()];
+                    }
+                    r
+                },
+                Two(f) => {
+                    let (oa, ob) = (~a.unwrap().clone(), ~b.unwrap().clone());
+                    let mut r = safe(proc() { f(*oa, *ob) }).result(g);
+                    if r.is_failure() {
+                        r.arguments = ~[a.to_str(), b.to_str()];
+                    }
+                    r
+                },
+                Three(f) => {
+                    let (oa, ob) = (~a.unwrap().clone(), ~b.unwrap().clone());
+                    let oc = ~c.unwrap().clone();
+                    let mut r = safe(proc() { f(*oa, *ob, *oc) }).result(g);
+                    if r.is_failure() {
+                        r.arguments = ~[a.to_str(), b.to_str(), c.to_str()];
+                    }
+                    r
+                },
             }
-            r
-        })
+        }
     }
 
-    fn apply2<A: AShow, B: AShow, C: Testable, G: Gen>
-             (g: &mut G, fun: |a: A, b: B| -> C) -> ~TestResult {
-        let (a, b): (A, B) = arby(g);
-        shrink(a, b, (), |a: A, b: B, _: ()| {
-            let mut r = fun(a.clone(), b.clone()).result(g);
-            if r.is_failure() {
-                r.arguments = ~[a.to_str(), b.to_str()];
-            }
-            r
-        })
-    }
-
-    fn apply3<A: AShow, B: AShow, C: AShow, D: Testable, G: Gen>
-             (g: &mut G, fun: |a: A, b: B, c: C| -> D) -> ~TestResult {
-        let (a, b, c): (A, B, C) = arby(g);
-        shrink(a, b, c, |a: A, b: B, c: C| {
-            let mut r = fun(a.clone(), b.clone(), c.clone()).result(g);
-            if r.is_failure() {
-                r.arguments = ~[a.to_str(), b.to_str(), c.to_str()];
-            }
-            r
-        })
-    }
-
-    fn shrink<A: AShow, B: AShow, C: AShow>
-             (a: A, b: B, c: C, fun: |A, B, C| -> ~TestResult)
+    fn shrink<G: Gen, A: AShow, B: AShow, C: AShow, T: Testable>
+             (g: &mut G, fun: Fun<A, B, C, T>)
              -> ~TestResult {
-        let r = fun(a.clone(), b.clone(), c.clone());
+        let (a, b, c): (A, B, C) = arby(g);
+        let r = fun.call(g, Some(&a), Some(&b), Some(&c));
         match r.status {
             Pass|Discard => r,
             Fail => {
                 // We've found a failing test case, so try to shrink it.
-                match shrink_failure((a, b, c).shrink(), fun) {
+                match shrink_failure(g, (a, b, c).shrink(), fun) {
                     Some(smaller) => smaller,
                     None => r,
                 }
@@ -322,12 +323,12 @@ mod tester {
         }
     }
 
-    fn shrink_failure<A: AShow, B: AShow, C: AShow>
-                     (mut shrinker: ~ObjIter:<(A, B, C)>,
-                      fun: |A, B, C| -> ~TestResult)
+    fn shrink_failure<G: Gen, A: AShow, B: AShow, C: AShow, T: Testable>
+                     (g: &mut G, mut shrinker: ~ObjIter:<(A, B, C)>,
+                      fun: Fun<A, B, C, T>)
                      -> Option<~TestResult> {
         for (a, b, c) in shrinker {
-            let r = fun(a.clone(), b.clone(), c.clone());
+            let r = fun.call(g, Some(&a), Some(&b), Some(&c));
             match r.status {
                 // The shrunk value does not witness a failure, so
                 // throw it away.
@@ -336,8 +337,7 @@ mod tester {
                 // The shrunk value *does* witness a failure, so keep trying
                 // to shrink it.
                 Fail => {
-                    let shrunk = shrink_failure((a, b, c).shrink(),
-                                                |a, b, c| fun(a, b, c));
+                    let shrunk = shrink_failure(g, (a, b, c).shrink(), fun);
 
                     // If we couldn't witness a failure on any shrunk value,
                     // then return the failure we already have.
@@ -348,14 +348,46 @@ mod tester {
         None
     }
 
-    // This is my bright idea for capturing runtime errors caused by a test.
-    // Haven't been able to get the compiler to accept it.
-    #[allow(dead_code)]
-    fn safe<T: Send>(fun: proc() -> T) -> Result<T, ~str> {
-        let tryr = task::try(proc() { fun() });
-        match tryr {
-            Ok(t) => Ok(t),
-            Err(_) => Err(~"WTF"),
+    #[cfg(quickfail)]
+    mod trap {
+        pub fn safe<T: Send>(fun: proc() -> T) -> Result<T, ~str> {
+            Ok(fun())
+        }
+    }
+
+    #[cfg(not(quickfail))]
+    mod trap {
+        use std::comm::Chan;
+        use std::io::{PortReader, ChanWriter};
+        use std::task::task;
+
+        // This is my bright idea for capturing runtime errors caused by a
+        // test. Actually, it looks like rustc uses a similar approach.
+        // The problem is, this is used for *each* test case passed to a
+        // property, whereas rustc does it once for each test.
+        //
+        // I'm not entirely sure there's much of an alternative either.
+        // We could launch a single task and pass arguments over a channel,
+        // but the task would need to be restarted if it failed due to a
+        // runtime error. Since these are rare, it'd probably be more efficient
+        // then this approach, but it would also be more complex.
+        //
+        // Moreover, this feature seems to prevent an implementation of
+        // Testable for a stack closure type. *sigh*
+        pub fn safe<T: Send>(fun: proc() -> T) -> Result<T, ~str> {
+            let (recv, send) = Chan::new();
+            let (stdout, stderr) = (ChanWriter::new(send.clone()), ChanWriter::new(send));
+            let mut reader = PortReader::new(recv);
+
+            let mut t = task();
+            t.opts.name = Some((~"safefn").into_maybe_owned());
+            t.opts.stdout = Some(~stdout as ~Writer);
+            t.opts.stderr = Some(~stderr as ~Writer);
+
+            match t.try(fun) {
+                Ok(v) => Ok(v),
+                Err(_) => Err(reader.read_to_str().unwrap().trim().into_owned()),
+            }
         }
     }
 
@@ -370,16 +402,49 @@ mod test {
     use std::iter;
     use std::vec;
     use rand::task_rng;
-    use super::{Config, Testable, TestResult, gen, quickcheck_config};
+    use super::{Config, Testable, TestResult, gen};
+    use super::{quickcheck_config, quicktest_config};
 
+    static SIZE: uint = 100;
     static CONFIG: Config = Config {
         tests: 100,
         max_tests: 10000,
     };
 
-    fn check<A: Testable>(f: A) {
-        quickcheck_config(CONFIG, &mut gen(task_rng(), 100), f)
+    fn qcheck<A: Testable>(f: A) {
+        quickcheck_config(CONFIG, &mut gen(task_rng(), SIZE), f)
     }
+
+    fn qtest<A: Testable>(f: A) -> Result<uint, TestResult> {
+        quicktest_config(CONFIG, &mut gen(task_rng(), SIZE), f)
+    }
+
+    #[test]
+    fn prop_oob() {
+        fn prop() -> bool {
+            (~[])[0]
+        }
+        match qtest(prop) {
+            Ok(n) => fail!("prop_oob should fail with a runtime error \
+                            but instead it passed {} tests.", n),
+            _ => return,
+        }
+    }
+
+    #[test]
+    fn simple_closure() {
+        qcheck(|| true);
+    }
+
+    // #[test] 
+    // fn prop_reverse_reverse_closure() { 
+        // let prop = |xs: ~[uint]| -> bool { 
+            // let rev = xs.clone().move_rev_iter().to_owned_vec(); 
+            // let revrev = rev.move_rev_iter().to_owned_vec(); 
+            // xs == revrev 
+        // }; 
+        // qcheck(prop); 
+    // } 
 
     #[test]
     fn prop_reverse_reverse() {
@@ -388,7 +453,7 @@ mod test {
             let revrev = rev.move_rev_iter().to_owned_vec();
             xs == revrev
         }
-        check(prop);
+        qcheck(prop);
     }
 
     #[test]
@@ -401,7 +466,7 @@ mod test {
                 xs == xs.clone().move_rev_iter().to_owned_vec()
             )
         }
-        check(prop);
+        qcheck(prop);
     }
 
     #[test]
@@ -416,7 +481,7 @@ mod test {
 
             app_rev == rev_app
         }
-        check(prop);
+        qcheck(prop);
     }
 
     #[test]
@@ -428,7 +493,7 @@ mod test {
                 return TestResult::from_bool(::std::cmp::max(x, y) == y)
             }
         }
-        check(prop);
+        qcheck(prop);
     }
 
     #[test]
@@ -443,7 +508,7 @@ mod test {
             }
             true
         }
-        check(prop);
+        qcheck(prop);
     }
 
     #[test]
@@ -487,6 +552,6 @@ mod test {
             }
             return true
         }
-        check(prop);
+        qcheck(prop);
     }
 }
