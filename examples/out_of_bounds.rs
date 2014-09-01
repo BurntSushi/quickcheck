cab67a1a2ebdd39b90d32f177d88b2691fcb9aa4
Jorge Aparicio <japaric@linux.com>
2014-09-01 10:48:45 -0500

    Add TestResult::must_fail
    Closes #18

---
+++ examples/out_of_bounds.rs
@@ -0,0 +1,18 @@
+extern crate quickcheck;
+
+use quickcheck::{TestResult, quickcheck};
+
+fn main() {
+    fn prop(length: uint, index: uint) -> TestResult {
+        let v = Vec::from_fn(length, |i| i);
+
+        if index < length {
+            TestResult::discard()
+        } else {
+            TestResult::must_fail(proc() {
+                v[index]
+            })
+        }
+    }
+    quickcheck(prop);
+}
