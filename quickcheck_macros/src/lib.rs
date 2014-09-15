8c2f643a3a8f66e36a05318cd79fbb54b3c35d7b
Andrew Gallant <jamslam@gmail.com>
2014-09-15 11:41:25 -0400

    More changes to bring up to date with master.

--- quickcheck_macros/src/lib.rs
+++ quickcheck_macros/src/lib.rs
@@ -11,13 +11,12 @@
 extern crate syntax;
 extern crate rustc;
 
-use std::gc::{GC, Gc};
-
 use syntax::ast;
 use syntax::codemap;
 use syntax::parse::token;
 use syntax::ext::base::{ExtCtxt, ItemModifier};
 use syntax::ext::build::AstBuilder;
+use syntax::ptr::P;
 
 use rustc::plugin::Registry;
 
@@ -50,18 +49,18 @@ pub fn plugin_registrar(reg: &mut Registry) {
 /// ```
 fn expand_meta_quickcheck(cx: &mut ExtCtxt,
                           span: codemap::Span,
-                          _: Gc<ast::MetaItem>,
-                          item: Gc<ast::Item>) -> Gc<ast::Item> {
+                          _: &ast::MetaItem,
+                          item: P<ast::Item>) -> P<ast::Item> {
     match item.node {
         ast::ItemFn(..) | ast::ItemStatic(..) => {
             // Copy original function without attributes
-            let prop = box(GC) ast::Item {attrs: Vec::new(), ..(*item).clone()};
+            let prop = P(ast::Item {attrs: Vec::new(), ..(*item).clone()});
             // ::quickcheck::quickcheck
             let check_ident = token::str_to_ident("quickcheck");
             let check_path = vec!(check_ident, check_ident);
             // Wrap original function in new outer function, calling ::quickcheck::quickcheck()
-            let fn_decl = box(GC) codemap::respan(span, ast::DeclItem(prop));
-            let inner_fn = box(GC) codemap::respan(span, ast::StmtDecl(fn_decl, ast::DUMMY_NODE_ID));
+            let fn_decl = P(codemap::respan(span, ast::DeclItem(prop.clone())));
+            let inner_fn = P(codemap::respan(span, ast::StmtDecl(fn_decl, ast::DUMMY_NODE_ID)));
             let inner_ident = cx.expr_ident(span, prop.ident);
             let check_call = cx.expr_call_global(span, check_path, vec![inner_ident]);
             let body = cx.block(span, vec![inner_fn], Some(check_call));
@@ -72,7 +71,7 @@ fn expand_meta_quickcheck(cx: &mut ExtCtxt,
             // Add #[test] attribute
             attrs.push(cx.attribute(span, cx.meta_word(span, token::intern_and_get_ident("test"))));
             // Attach the attributes to the outer function
-            box(GC) ast::Item {attrs: attrs, ..(*test).clone()}
+            P(ast::Item {attrs: attrs, ..(*test).clone()})
         },
         _ => {
             cx.span_err(span, "#[quickcheck] only supported on statics and functions");
