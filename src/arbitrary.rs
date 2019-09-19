9acb60f1b7d19a8aa11ff0d6187262848c266611
Max Blachman <blachmanmax@gmail.com>
2019-09-19 01:34:16 -0700

    switch from `%` to `gen_range()` for Duration
    This ensures the distribution will be uniform, rather than just close
    to uniform (because of problem values).

--- src/arbitrary.rs
+++ src/arbitrary.rs
@@ -868,8 +868,8 @@ impl Arbitrary for RangeFull {
 
 impl Arbitrary for Duration {
     fn arbitrary<G: Gen>(gen: &mut G) -> Self {
-        let seconds = u64::arbitrary(gen) % (gen.size() as u64) ;
-        let nanoseconds = u32::arbitrary(gen) % 1_000_000;
+        let seconds = gen.gen_range(0, gen.size() as u64);
+        let nanoseconds = gen.gen_range(0, 1_000_000);
         Duration::new(seconds, nanoseconds)
     }
 
